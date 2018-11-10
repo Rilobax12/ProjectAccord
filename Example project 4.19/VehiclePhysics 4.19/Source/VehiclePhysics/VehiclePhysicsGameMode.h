c590ba8cd3e6a4e8e7a0dec633ff4521e482ccec
Bankworthy <aidan.cadogan@hotmail.co.uk>
2018-11-10 00:00:07 +0000

    Initial project commit

---
+++ Example project 4.19/VehiclePhysics 4.19/Source/VehiclePhysics/VehiclePhysicsGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "VehiclePhysicsGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VEHICLEPHYSICS_API AVehiclePhysicsGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
